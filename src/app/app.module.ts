5e410c5f36dce2cec4cad7ff73cb9b5d8fda5739
devwilian <wdarioh95@gmail.com>
2020-11-02 20:09:22 -0500

    Instalacion de librerias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { FormularioComponent } from './components/form/formulario/formulario.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FormularioComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
