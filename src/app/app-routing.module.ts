c492a95836ab01f9148abea6f11d5824cf95719d
devwilian <wdarioh95@gmail.com>
2020-12-02 20:19:27 -0500

    prueba formularioReclamos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormularioComponent } from './components/form/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'formulario',component:FormularioComponent},
+  {path:'',pathMatch:'full',redirectTo:'formulario'},
+  {path:'**',pathMatch:'full',redirectTo:'formulario'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
